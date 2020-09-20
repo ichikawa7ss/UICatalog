bda387e7cf11d6548b43f93da85d6ad95058bde5
ichikawa7ss <i2s7.chacha@gmail.com>
2020-09-20 23:38:31 +0900

    Impl search view sliding animation.

--- UICatalog/UICatalog/Views/SearchAnimation/SearchAnimationViewController.swift
+++ UICatalog/UICatalog/Views/SearchAnimation/SearchAnimationViewController.swift
@@ -10,7 +10,66 @@ import UIKit
 
 final class SearchAnimationViewController: UIViewController {
     
+    @IBOutlet private weak var searchButton: UIView! {
+        willSet {
+            let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self,  action: #selector(SearchAnimationViewController.tapped(_:)))
+            newValue.addGestureRecognizer(tapGesture)
+        }
+    }
+    @IBOutlet private weak var searchButtonWidthConstraints: NSLayoutConstraint!
+    @IBOutlet private weak var searchButtonTrailingConstraints: NSLayoutConstraint!
+    @IBOutlet private weak var textFiledWidthConstraints: NSLayoutConstraint!
+    @IBOutlet private weak var textFiledTrailingConstraints: NSLayoutConstraint!
+    @IBOutlet private weak var headerLabel: UILabel!
+    @IBOutlet private weak var headerView: UIView!
+    
+    
+    let searchButtonWidth = 32.0
+    var searchButtonAnimationStyle: ButtonAnimationStyle = .expand
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    @objc func tapped(_ sender: UITapGestureRecognizer) {
+        updateSearchButtonLayout(style: self.searchButtonAnimationStyle)
+        UIView.animate(withDuration: 0.3, delay: 0.0, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+}
+
+extension SearchAnimationViewController {
+    
+    private func updateSearchButtonLayout(style: ButtonAnimationStyle) {
+        switch style {
+        case .expand:
+            self.textFiledWidthConstraints.isActive = true
+            self.textFiledTrailingConstraints.isActive = true
+            self.searchButtonWidthConstraints.isActive = false
+            self.searchButtonTrailingConstraints.isActive = false
+        case .shrink:
+            self.textFiledWidthConstraints.isActive = false
+            self.textFiledTrailingConstraints.isActive = false
+            self.searchButtonWidthConstraints.isActive = true
+            self.searchButtonTrailingConstraints.isActive = true
+        }
+        self.searchButtonAnimationStyle = searchButtonAnimationStyle.getOpposite()
+    }
+}
+
+enum ButtonAnimationStyle {
+    
+    case expand
+    case shrink
+    
+    func getOpposite() -> ButtonAnimationStyle {
+        switch self {
+        case .expand:
+            return .shrink
+        case .shrink:
+            return .expand
+        }
     }
 }
