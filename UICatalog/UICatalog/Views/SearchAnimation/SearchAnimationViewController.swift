a478df48bc69334c312e19c3918de177bbea5cf1
ichikawa7ss <i2s7.chacha@gmail.com>
2020-09-23 11:02:52 +0900

    Add BackToMenuButton.

--- UICatalog/UICatalog/Views/SearchAnimation/SearchAnimationViewController.swift
+++ UICatalog/UICatalog/Views/SearchAnimation/SearchAnimationViewController.swift
@@ -11,32 +11,45 @@ import UIKit
 final class SearchAnimationViewController: UIViewController {
     
     @IBOutlet private weak var slideSearchHeaderView: SlideSearchHeaderView!
+    @IBOutlet private weak var profileView: UIImageView!
+    @IBOutlet weak var backMenuButton: UIButton! {
+        willSet {
+            newValue.setImage(UIImage(systemName: "arrowshape.turn.up.left.circle.fill"), for: .normal)
+            newValue.tintColor = .black
+            newValue.contentVerticalAlignment = .fill
+            newValue.contentHorizontalAlignment = .fill
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
         self.slideSearchHeaderView.delegate = self
     }
+    
+    @IBAction func tapBackToMenuButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
 
 extension SearchAnimationViewController: SlideSearchHeaderViewDelegate {
     
     func willStartExpandAnimation(_ searchHeaderView: SlideSearchHeaderView) {
-        print("willExpand")
+        self.profileView.image = UIImage(named: "SearchingView")
+        self.backMenuButton.isHidden = true
     }
     
     func willStartShrinkAnimation(_ searchHeaderView: SlideSearchHeaderView) {
-        print("willShrink")
+        self.profileView.image = UIImage(named: "ProfileView")
+        self.backMenuButton.isHidden = false
     }
     
     func slideSearchHeaderView(_ searchHeaderView: SlideSearchHeaderView, didEnterTextField: UITextField) {
-        print(didEnterTextField.text)
-        print("didEnterButton")
+        // NOP
     }
     
     func slideSearchHeaderView(_ searchHeaderView: SlideSearchHeaderView, didChangeCharacters: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) {
-        print(didChangeCharacters.text)
-        print("didChangeCharacters")
+        // NOP
     }
 }
 
