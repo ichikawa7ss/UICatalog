832c58237894f3bb2fa688e5ca0b6f73df5ae01b
ichikawa7ss <i2s7.chacha@gmail.com>
2020-10-14 23:47:45 +0900

    Add RightLargeCell.

--- UICatalog/UICatalog/Views/RegularCollectionLayout/RegularCollectionLayoutViewController.swift
+++ UICatalog/UICatalog/Views/RegularCollectionLayout/RegularCollectionLayoutViewController.swift
@@ -15,6 +15,7 @@ final class RegularCollectionLayoutViewController: UIViewController {
             newValue.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 0, right: 0)
             newValue.register(LinerSmallTableCell.self)
             newValue.register(LeftLargeTableCell.self)
+            newValue.register(RightLargeTableCell.self)
         }
     }
     
@@ -25,16 +26,19 @@ final class RegularCollectionLayoutViewController: UIViewController {
 
 extension RegularCollectionLayoutViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let num = indexPath.row % 3
-        if num == 0 {
+        let num = indexPath.row % 4
+        if num == 0 || num == 2 {
             let cell = self.linerSmallTableCell(tableView.dequeueReusableCell(for: indexPath))
             return cell
         } else if num == 1 {
             let cell = self.leftLargeTableCell(tableView.dequeueReusableCell(for: indexPath))
+            return cell
+        } else if num == 3 {
+            let cell = self.rightLargeTableCell(tableView.dequeueReusableCell(for: indexPath))
             return cell
         } else {
             return UITableViewCell()
@@ -47,6 +51,11 @@ extension RegularCollectionLayoutViewController: UITableViewDataSource {
     }
     
     private func leftLargeTableCell(_ cell: LeftLargeTableCell) -> LeftLargeTableCell {
+        // TODO: - Implement
+        return cell
+    }
+    
+    private func rightLargeTableCell(_ cell: RightLargeTableCell) -> RightLargeTableCell {
         // TODO: - Implement
         return cell
     }
