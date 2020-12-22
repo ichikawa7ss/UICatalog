d230145365363c2e70331506de63399614876993
ichikawa7ss <i2s7.chacha@gmail.com>
2020-12-22 09:28:25 +0900

    Impl SwitchableViewController.

---
+++ UICatalog/UICatalog/Views/SwichableViewController/Root/Child/Search/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  UICatalog
+//
+//  Created by ichikawa on 2020/12/22.
+//  Copyright © 2020 ichikawa. All rights reserved.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
