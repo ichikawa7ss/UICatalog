dbadd503cf05142702f486b413992595b7c674db
ichikawa7ss <i2s7.chacha@gmail.com>
2021-03-24 12:28:07 +0900

    bind parent view controller with last digit inputed

--- UICatalog/UICatalog/Views/DigitInputView/DigitInputViewController.swift
+++ UICatalog/UICatalog/Views/DigitInputView/DigitInputViewController.swift
@@ -15,5 +15,24 @@ final class DigitInputViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.digitInputView.configure()
+        self.digitInputView.delegate = self
+    }
+}
+
+extension DigitInputViewController: DiditInputViewDelegate {
+
+    func digitInputView(_ digitInputView: DigitInputView, didFinishInput text: String) {
+        let alert = UIAlertController(
+            title          : "認証コードが入力されました",
+            message        : "認証コード: \(text)",
+            preferredStyle : .alert
+        )
+        alert.addAction(UIAlertAction(
+            title   : "OK",
+            style   : .default,
+            handler : { _ in
+                self.navigationController?.popViewController(animated: true)
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
 }
--- UICatalog/UICatalog/Views/DigitInputView/Parts/DigitInputView/DigitInputView.swift
+++ UICatalog/UICatalog/Views/DigitInputView/Parts/DigitInputView/DigitInputView.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
+protocol DiditInputViewDelegate: AnyObject {
+    func digitInputView(_ digitInputView: DigitInputView, didFinishInput text: String)
+}
+
 final class DigitInputView: BaseView {
 
     @IBOutlet private var digitInputTextFields: [DigitInputTextField]!
+    weak var delegate: DiditInputViewDelegate?
     
     func configure() {
-        self.digitInputTextFields.forEach { $0.configure() }
+        self.digitInputTextFields.forEach { textField in
+            textField.configure()
+            textField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        }
+        _ = self.digitInputTextFields.filter { $0.tag == 0 }.map { $0.becomeFirstResponder() } // 最初のテキストにフォーカスしておく
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if textField.tag != 5 {
+            // 最終タグ以外
+            _ = self.digitInputTextFields.filter { $0.tag == textField.tag + 1 }.map { $0.becomeFirstResponder() }
+        }
+        else {
+            // 最終タグはdelegateで親のViewControllerに通知
+            guard self.digitInputTextFields.allSatisfy({ $0.isInputed }) else {
+                return
+            }
+            self.delegate?.digitInputView(self, didFinishInput: self.digitInputTextFields.compactMap { $0.text }.joined())
+        }
     }
 }
 
@@ -33,10 +56,15 @@ final class DigitInputTextField: UITextField {
         return false
     }
     
+    var isInputed: Bool {
+        return !(self.text?.isEmpty ?? true)
+    }
+    
     // プロパティの設定
     func configure() {
         self.textColor = .blue
         self.font = UIFont(name: "HiraginoSans-W6", size: 48)!
         self.textAlignment = .center
+        self.keyboardType = .numberPad
     }
 }
