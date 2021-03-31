236da2877edf32a46d5747d41b4464c37593ef91
ichikawa7ss <i2s7.chacha@gmail.com>
2021-03-31 11:29:57 +0900

    add child components

--- UICatalog/UICatalog/Views/ShrinkingModalAnimation/Child/ShrinkingModalChildViewController.swift
+++ UICatalog/UICatalog/Views/ShrinkingModalAnimation/Child/ShrinkingModalChildViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 final class ShrinkingModalChildViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView! {
+        willSet {
+            newValue.register(ShrinkingCell.self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func didTapCloseButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ShrinkingModalChildViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.shrinkingCell(tableView.dequeueReusableCell(for: indexPath))
+        return cell
+    }
+    
+    private func shrinkingCell(_ cell: ShrinkingCell) -> ShrinkingCell {
+        cell.configure()
+        return cell
     }
 }
