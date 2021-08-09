2a5c9d1fce6d2b8934e785f5e09316cd7f36f87c
ichikawa7ss <i2s7.chacha@gmail.com>
2021-08-09 16:21:33 +0900

    add SmoothSelectableTableView.

--- UICatalog/UICatalog/Views/SmoothSelectableTableView/SmoothSelectableViewController.swift
+++ UICatalog/UICatalog/Views/SmoothSelectableTableView/SmoothSelectableViewController.swift
@@ -10,7 +10,30 @@ import UIKit
 
 final class SmoothSelectableViewController: UIViewController {
     
+    @IBOutlet private weak var tableView: SmoothSelectableTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.setupLongPress(self)
+    }
+}
+
+extension SmoothSelectableViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SmoothSelectableCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row + 1)番目のセル"
+        return cell
+    }
+}
+
+extension SmoothSelectableViewController: SmoothSelectableTableViewDelegate {
+ 
+    func smoothSelectableTableView(_ tableView: SmoothSelectableTableView, didUpdateLongPressState state: SmoothSelectalbeTableViewState) {
+        print("didUpdateLongPressState: \(state)")
     }
 }
