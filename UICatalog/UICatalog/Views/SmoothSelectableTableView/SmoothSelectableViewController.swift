8b749c6838702b1e892b576c59cb2bf48932fd09
ichikawa7ss <i2s7.chacha@gmail.com>
2021-08-09 17:31:43 +0900

    handle selected index in ViewController.

--- UICatalog/UICatalog/Views/SmoothSelectableTableView/SmoothSelectableViewController.swift
+++ UICatalog/UICatalog/Views/SmoothSelectableTableView/SmoothSelectableViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 final class SmoothSelectableViewController: UIViewController {
     
     @IBOutlet private weak var tableView: SmoothSelectableTableView!
+    private var selectedIndex: Set<IndexPath> = []
+    private var longPreesedIndex: Set<IndexPath> = []
+    private var isSelecting: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +30,9 @@ extension SmoothSelectableViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SmoothSelectableCell", for: indexPath)
         cell.textLabel?.text = "\(indexPath.row + 1)番目のセル"
+        let selectedAllIndex = self.isSelecting ? self.selectedIndex.union(self.longPreesedIndex) : self.selectedIndex.subtracting(self.longPreesedIndex)
+        let isSelected = !selectedAllIndex.filter { $0 == indexPath }.isEmpty
+        cell.backgroundColor = isSelected ? .cyan : .white
         return cell
     }
 }
@@ -34,6 +40,27 @@ extension SmoothSelectableViewController: UITableViewDataSource {
 extension SmoothSelectableViewController: SmoothSelectableTableViewDelegate {
  
     func smoothSelectableTableView(_ tableView: SmoothSelectableTableView, didUpdateLongPressState state: SmoothSelectalbeTableViewState) {
-        print("didUpdateLongPressState: \(state)")
+        switch state {
+        case .began(let indexPath):
+            self.isSelecting = !self.selectedIndex.contains(indexPath)
+            self.updateSelectedIndex(indexPath)
+        case .changed(let indexPaths):
+            self.longPreesedIndex = []
+            indexPaths.forEach { indexPath in
+                self.updateSelectedIndex(indexPath)
+            }
+        case .ended(let indexPath):
+            self.updateSelectedIndex(indexPath)
+            self.selectedIndex = self.isSelecting ? self.selectedIndex.union(self.longPreesedIndex) : self.selectedIndex.subtracting(self.longPreesedIndex)
+            self.longPreesedIndex = []
+        }
+    }
+}
+
+extension SmoothSelectableViewController {
+
+    private func updateSelectedIndex(_ index: IndexPath) {
+        self.longPreesedIndex.insert(index)
+        self.tableView.reloadData()
     }
 }
